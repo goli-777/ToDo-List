1718b8ecbd1c70f630747abe6d21d8722e2610e6
goli-777 <goli.jafariyan84@gmail.com>
2025-01-18 23:25:08 +0330

    creat a textbox when add button is pushed

---
+++ TodoList/TodoList/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace TodoList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.AddTaskBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.monthCalendar1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.monthCalendar1.Location = new System.Drawing.Point(47, 28);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.ShowToday = false;
+            this.monthCalendar1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(1, 258);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(416, 23);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "________________________________________________________________________________";
+            // 
+            // AddTaskBtn
+            // 
+            this.AddTaskBtn.BackColor = System.Drawing.Color.Gold;
+            this.AddTaskBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AddTaskBtn.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.AddTaskBtn.Location = new System.Drawing.Point(332, 576);
+            this.AddTaskBtn.Name = "AddTaskBtn";
+            this.AddTaskBtn.Size = new System.Drawing.Size(60, 55);
+            this.AddTaskBtn.TabIndex = 3;
+            this.AddTaskBtn.Text = "+";
+            this.AddTaskBtn.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(404, 667);
+            this.Controls.Add(this.AddTaskBtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.monthCalendar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button AddTaskBtn;
+    }
+}
+
---
+++ TodoList/TodoList/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TodoList
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void AddTaskBtn_Click(object sender, EventArgs e)
+        {
+            TextBox txtbox = new TextBox();
+            this.Controls.Add(txtbox);
+            txtbox.Multiline = true;
+            txtbox.Width = 380;
+            txtbox.Name = "txtbox1";
+           txtbox.Location = new Point(12, 303);
+            
+        }
+    }
+}
